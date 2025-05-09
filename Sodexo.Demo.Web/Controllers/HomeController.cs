afa607d97772b2e4acee3f5511563f89d45dc357
Uzua <uzuajcvd@gmail.com>
2025-05-09 08:38:29 -0500

    Se agregó sección de Contacto y enlace en el menú de navegación

--- Sodexo.Demo.Web/Controllers/HomeController.cs
+++ Sodexo.Demo.Web/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace Sodexo.Demo.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
     }
 }
