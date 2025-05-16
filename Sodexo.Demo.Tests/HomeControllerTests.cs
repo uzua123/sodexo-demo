73c9e5b7f65252d7581a0c8e06eff11877894e79
Uzua <uzuajcvd@gmail.com>
2025-05-16 10:05:07 -0500

    Se agregó el proyecto TEST

---
+++ Sodexo.Demo.Tests/HomeControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Sodexo.Demo.Web.Controllers;
+
+namespace Sodexo.Demo.Tests
+{
+    public class HomeControllerTests
+    {
+        private readonly Mock<ILogger<HomeController>> _mockLogger;
+        private readonly HomeController _controller;
+
+        public HomeControllerTests()
+        {
+            _mockLogger = new Mock<ILogger<HomeController>>();
+            _controller = new HomeController(_mockLogger.Object);
+        }
+
+        [Fact]
+        public void Index_Returns_ViewResult()
+        {
+            // Act
+            var result = _controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Privacy_Returns_ViewResult()
+        {
+            // Act
+            var result = _controller.Privacy();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Contacto_Returns_ViewResult()
+        {
+            // Act
+            var result = _controller.Contacto();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Error_Returns_ViewResult_With_ErrorViewModel()
+        {
+            // Act
+            var result = _controller.Error();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
--- Sodexo.Demo.Web/Controllers/HomeController.cs
+++ Sodexo.Demo.Web/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Sodexo.Demo.Web.Models;
 
 namespace Sodexo.Demo.Web.Controllers
 {
@@ -26,7 +24,7 @@ namespace Sodexo.Demo.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
 
         public IActionResult Contacto()
